d83f92025aa887306d425de4534a64d5c524bcdf
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-09-04 22:31:11 -0700

    override the OnModelCreating method of the base class by giving instructions how to build CatalogBrand table

--- ProductCatalogApi/Data/CatalogContext.cs
+++ ProductCatalogApi/Data/CatalogContext.cs
@@ -19,7 +19,20 @@ namespace ProductCatalogApi.Data
         public DbSet<CatalogType> CatalogTypes { get; set; }
         public DbSet<CatalogItem> CatalogItems { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CatalogBrand>(e =>
+            {
+                e.ToTable("CatalogBrands");
+                e.Property(b => b.Id)
+                    .IsRequired()
+                    .UseHiLo("catalog_brand_hilo");
 
+                e.Property(b => b.Brand)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+        }
 
     }
 }
