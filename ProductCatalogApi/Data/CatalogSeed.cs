c6249834912baed3c0e9c48e40b8f434015a75ed
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-09-07 21:40:21 -0700

    add CatalogSeed class responsible to populate tables, add line of code to update automatically once data has been migrated into database

---
+++ ProductCatalogApi/Data/CatalogSeed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogApi.Data
+{
+    public static class CatalogSeed
+    {
+        public static void Seed(CatalogContext context)
+        {
+            context.Database.Migrate();
+
+        }
+    }
+}
