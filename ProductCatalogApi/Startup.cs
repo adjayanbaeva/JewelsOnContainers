a82366a7b0db9298c259c3216b4b2ca6a4aae940
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-09-07 20:17:29 -0700

    in Startup.cs inject database into service

--- ProductCatalogApi/Startup.cs
+++ ProductCatalogApi/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProductCatalogApi.Data;
 
 namespace ProductCatalogApi
 {
@@ -25,6 +27,8 @@ namespace ProductCatalogApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<CatalogContext>(options=>
+                options.UseSqlServer(Configuration["ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
