6a182822fbc5484b472cffe8b42e4a3f65521501
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-09-08 22:10:21 -0700

    populate CatalogType table with data

--- ProductCatalogApi/Data/CatalogSeed.cs
+++ ProductCatalogApi/Data/CatalogSeed.cs
@@ -15,8 +15,26 @@ namespace ProductCatalogApi.Data
             if(!context.CatalogBrands.Any())
             {
                 context.CatalogBrands.AddRange(GetPreConfiguredCatalogBrands());
+                context.SaveChanges();
             }
 
+            if (!context.CatalogTypes.Any())
+            {
+                context.CatalogTypes.AddRange(GetPreConfiguredCatalogTypes());
+                context.SaveChanges();
+            }
+
+
+        }
+
+        private static IEnumerable<CatalogType> GetPreConfiguredCatalogTypes()
+        {
+            return new List<CatalogType>
+            {
+                new CatalogType {Type="Engagement Ring"},
+                new CatalogType {Type="Wedding Ring"},
+                new CatalogType {Type="Fashion Ring"}
+            };
         }
 
         private static IEnumerable<CatalogBrand> GetPreConfiguredCatalogBrands()
