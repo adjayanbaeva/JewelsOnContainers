48f9bd6eb8e48f1ba3dbde494cc0d30394d7071f
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-09-15 22:59:15 -0700

    create a CatalogController and make a dependency injection in its constructor

---
+++ ProductCatalogApi/Controllers/CatalogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductCatalogApi.Data;
+
+namespace ProductCatalogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly CatalogContext _context;
+        public CatalogController(CatalogContext context)
+        {
+            _context = context;
+        }
+    }
+}
