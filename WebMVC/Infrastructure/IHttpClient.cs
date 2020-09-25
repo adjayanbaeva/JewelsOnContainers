ccc4adb395c4154a56b9f6f11fccbfd2f3f3618a
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-09-24 22:03:28 -0700

    create IHttpClient interface and add GetStringAsync contract

---
+++ WebMVC/Infrastructure/IHttpClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Infrastructure
+{
+    public interface IHttpClient
+    {
+        Task<string> GetStringAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer");
+    }
+}
