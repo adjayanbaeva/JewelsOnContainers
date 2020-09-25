1609303b28085f272a2a0933c76512e825b19f14
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-09-24 22:14:37 -0700

    add PostAsync method in IHttpClient

--- WebMVC/Infrastructure/IHttpClient.cs
+++ WebMVC/Infrastructure/IHttpClient.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace WebMVC.Infrastructure
 {
     public interface IHttpClient
     {
-        Task<string> GetStringAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer");
+        Task<string> GetStringAsync(string uri, 
+            string authorizationToken = null, 
+            string authorizationMethod = "Bearer");
+        Task<HttpResponseMessage> PostAsync<T>(string uri, 
+            T item, 
+            string authorizarionToken = null, 
+            string authorizationMethod = "Bearer");
     }
 }
