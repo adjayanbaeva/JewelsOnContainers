a3953c4adad55d14184aa3912a7e7c54e3a56924
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-09-28 23:07:10 -0700

    add a CatalogService that will implement ICatalogService interface

--- WebMVC/Services/CatalogService.cs
+++ WebMVC/Services/CatalogService.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using WebMVC.Models;
 
 namespace WebMVC.Services
 {
-    public interface ICatalogService
+    public class CatalogService : ICatalogService
     {
-        Task<Catalog> GetCatalogItemsAsync(int page, int size);
     }
 }
