5bb86fdb89529b1b63fd280af18dfcb7fa56f823
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-09-28 23:25:07 -0700

    inject config into the constructor of CatalogService class and get the baseUri, define catalogItemsUri

--- WebMVC/Services/CatalogService.cs
+++ WebMVC/Services/CatalogService.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMVC.Infrastructure;
+using WebMVC.Models;
 
 namespace WebMVC.Services
 {
     public class CatalogService : ICatalogService
     {
+        private readonly string _baseUri;
+        public CatalogService(IConfiguration config)
+        {
+            _baseUri = $"{ config["CatalogUrl"]}/api/catalog/";
+        }
+        public async Task<Catalog> GetCatalogItemsAsync(int page, int size)
+        {
+            var catalogItemsUri = ApiPaths.Catalog.GetAllCatalogItems(_baseUri, page, size);
+        }
     }
 }
