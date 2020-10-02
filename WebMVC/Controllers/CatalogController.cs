8d336efa2461f48dc100ab05191276ad1d6fb3c4
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-10-01 22:31:48 -0700

    add a constructor to CatalogController and inject ICatalogService

--- WebMVC/Controllers/CatalogController.cs
+++ WebMVC/Controllers/CatalogController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebMVC.Services;
 
 namespace WebMVC.Controllers
 {
     public class CatalogController : Controller
     {
+        private readonly ICatalogService _service;
+        public CatalogController(ICatalogService service)
+        {
+            _service = service;
+        }
+
         public IActionResult Index()
         {
             var itemsOnPage = 10;
