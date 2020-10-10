dbe415e13d502f4c98c5e27a452d4e8e932f397b
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-10-09 21:26:29 -0700

    as part of implementation of ICatalogService interface get the uri catalog brands and make an http call

--- WebMVC/Services/CatalogService.cs
+++ WebMVC/Services/CatalogService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -18,13 +19,24 @@ namespace WebMVC.Services
             _baseUri = $"{ config["CatalogUrl"]}/api/catalog/";
             _client = client;
         }
+
         public async Task<Catalog> GetCatalogItemsAsync(int page, int size)
         {
             var catalogItemsUri = ApiPaths.Catalog.GetAllCatalogItems(_baseUri, page, size);
             var dataString = await _client.GetStringAsync(catalogItemsUri);
             return JsonConvert.DeserializeObject<Catalog>(dataString);
+        }
 
+        public async Task<IEnumerable<SelectListItem>> GetBrandsAsync()
+        {
+            var typeUri = ApiPaths.Catalog.GetAllBrands(_baseUri);
+            var dataString = await _client.GetStringAsync(typeUri);
+            
+        }
 
+        public Task<IEnumerable<SelectListItem>> GetTypesAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
