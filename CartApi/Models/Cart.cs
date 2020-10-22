1933a1cd200a36f4762924d00ccd873629145b3a
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-10-21 22:23:59 -0700

    add a constructor for Cart class

--- CartApi/Models/Cart.cs
+++ CartApi/Models/Cart.cs
@@ -9,5 +9,11 @@ namespace CartApi.Models
     {
         public string BuyerId { get; set; }
         public List<CartItem> Items { get; set; }
+
+        public Cart(string cartId)
+        {
+            BuyerId = cartId;
+            Items = new List<CartItem>();
+        }
     }
 }
