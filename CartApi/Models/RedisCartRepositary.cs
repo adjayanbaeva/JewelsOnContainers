873493e6143bd4ca173a58bec124f6c659fa1548
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-10-31 23:43:39 -0700

    inject the connection to redis in the constructor of RedisCartRepository class

--- CartApi/Models/RedisCartRepositary.cs
+++ CartApi/Models/RedisCartRepositary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StackExchange.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,32 @@ namespace CartApi.Models
 {
     public class RedisCartRepositary : ICartRepositary
     {
+        private readonly ConnectionMultiplexer _redis;
+        private readonly IDatabase _database;
+
+        public RedisCartRepositary(ConnectionMultiplexer redis)
+        {
+            _redis = redis;
+            _database = _redis.GetDatabase();
+        }
+        public Task<bool> DeleteCartAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cart> GetCartAsync(string cartId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cart> UpdateCartAsync(Cart basket)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
