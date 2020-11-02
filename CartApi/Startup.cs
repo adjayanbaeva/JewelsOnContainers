a2479fc29a2c63c4e7a2af72cb6fc3e3dda93e25
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-01 18:47:21 -0800

    in Startup.cs of Cart project inject RedisCartRepository anywhere where its interface is used

--- CartApi/Startup.cs
+++ CartApi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CartApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace CartApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddTransient<ICartRepositary, RedisCartRepositary>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
