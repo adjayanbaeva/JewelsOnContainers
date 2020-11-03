908d33e2c124954f70e3d6290313a02f03a9c2f0
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-02 17:29:08 -0800

    inject ICartRepository into the constructor of CartController

--- CartApi/Controllers/CartController.cs
+++ CartApi/Controllers/CartController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CartApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,10 @@ namespace CartApi.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
+        private readonly ICartRepositary _repository;
+        public CartController(ICartRepositary repository)
+        {
+            _repository = repository;
+        }
     }
 }
