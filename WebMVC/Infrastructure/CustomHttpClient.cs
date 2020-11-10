c647a4acadb1a3ddce7f611d7bb4fcbb5870a582
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-09 23:06:27 -0800

    add DoPostPutAsync private helper method to CustomHttpClient.cs

--- WebMVC/Infrastructure/CustomHttpClient.cs
+++ WebMVC/Infrastructure/CustomHttpClient.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -29,6 +31,37 @@ namespace WebMVC.Infrastructure
             }
             var response = await _client.SendAsync(requestMessage);
             return await response.Content.ReadAsStringAsync();
+        }
+
+        private async Task<HttpResponseMessage> DoPostPutAsync<T>(HttpMethod method, string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            if(method != HttpMethod.Post && method != HttpMethod.Put)
+            {
+                throw new ArgumentException("Value must be either post or put.", nameof(method));
+            }
+
+            // a new StringContent must be created for each retry 
+            // as it is disposed after each call
+
+            var requestMessage = new HttpRequestMessage(method, uri);
+            Console.WriteLine(JsonConvert.SerializeObject(item));
+            requestMessage.Content = new StringContent(JsonConvert.SerializeObject(item), System.Text.Encoding.UTF8, "application/json");
+            if (authorizationToken != null)
+            {
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue(authorizationMethod, authorizationToken);
+            }
+
+            var response = await _client.SendAsync(requestMessage);
+
+            // raise exception if HttpResponseCode 500 
+            // needed for circuit breaker to track fails
+
+            if (response.StatusCode == HttpStatusCode.InternalServerError)
+            {
+                throw new HttpRequestException();
+            }
+
+            return response;
         }
 
         public Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizarionToken = null, string authorizationMethod = "Bearer")
