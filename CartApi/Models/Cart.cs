29edee9cdb7efef4441274d6a817b6e6ff61a415
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-10 18:44:58 -0800

    add a parameterless constructor in Cart model of CartAPI project

--- CartApi/Models/Cart.cs
+++ CartApi/Models/Cart.cs
@@ -10,6 +10,9 @@ namespace CartApi.Models
         public string BuyerId { get; set; }
         public List<CartItem> Items { get; set; }
 
+        public Cart()
+        { }
+
         public Cart(string cartId)
         {
             BuyerId = cartId;
