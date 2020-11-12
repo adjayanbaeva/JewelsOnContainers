997287dd163f6d6460007198c7bbf9c950c4673a
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-11 20:59:10 -0800

    create CartService.cs class and inject needed dependencies into its constructor

---
+++ WebMVC/Services/CartService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVC.Infrastructure;
+
+namespace WebMVC.Services
+{
+    public class CartService : ICartService
+    {
+        private readonly IConfiguration _config;
+        private IHttpClient _apiClient;
+        private readonly string _remoteServiceBaseUrl;
+        private IHttpContextAccessor _httpContextAccessor;
+        private readonly ILogger _logger;
+        public CartService(IConfiguration config, IHttpContextAccessor httpContextAccessor, IHttpClient httpClient, ILoggerFactory logger)
+        {
+            _config = config;
+            _remoteServiceBaseUrl = $"{_config["CartUrl"]}/api/cart";
+            _httpContextAccessor = httpContextAccessor;
+            _apiClient = httpClient;
+            _logger = logger.CreateLogger<CartService>();
+
+        }
+    }
+}
