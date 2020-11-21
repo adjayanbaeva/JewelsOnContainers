5cec6e9085279285acca8006afa4c88c0eea81e9
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-20 21:57:55 -0800

    add InvokeAsync method in Cart.cs of ViewComponents

--- WebMVC/ViewComponents/Cart.cs
+++ WebMVC/ViewComponents/Cart.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Polly.CircuitBreaker;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebMVC.Models;
 using WebMVC.Services;
+using WebMVC.ViewModels;
 
 namespace WebMVC.ViewComponents
 {
@@ -17,7 +19,21 @@ namespace WebMVC.ViewComponents
         public async Task<IViewComponentResult> InvokeAsync(ApplicationUser user)
         {
             var vm = new CartComponentViewModel();
+            try
+            {
+                var cart = await _cartSvc.GetCart(user);
 
+                vm.ItemsInCart = cart.Items.Count;
+                vm.TotalCost = cart.Total();
+                return View(vm);
+            }
+            catch(BrokenCircuitException)
+            {
+                //Catch error when CartApi is in open circuit mode
+                ViewBag.IsBasketInoperative = true;
+            }
+
+            return View(vm);
         }
 
     }
--- WebMVC/ViewModels/CartComponentViewModel.cs
+++ WebMVC/ViewModels/CartComponentViewModel.cs
@@ -9,6 +9,6 @@ namespace WebMVC.ViewModels
     {
         public int ItemsInCart { get; set; }
         public decimal TotalCost { get; set; }
-        public string Disabled => (ItemsInCart == o) ? "is-disabled" : "";
+        public string Disabled => (ItemsInCart == 0) ? "is-disabled" : "";
     }
 }
