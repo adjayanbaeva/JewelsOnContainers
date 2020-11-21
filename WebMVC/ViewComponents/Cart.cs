4d650384ae35173e34ffb45bf6856e4580109322
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-20 21:50:07 -0800

    create CartComponentViewModel view model and define its properties

--- WebMVC/ViewComponents/Cart.cs
+++ WebMVC/ViewComponents/Cart.cs
@@ -3,10 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMVC.Models;
+using WebMVC.Services;
 
 namespace WebMVC.ViewComponents
 {
     public class Cart : ViewComponent
     {
+        private readonly ICartService _cartSvc;
+
+        public Cart(ICartService cartSvc) => _cartSvc = cartSvc;
+
+        public async Task<IViewComponentResult> InvokeAsync(ApplicationUser user)
+        {
+            var vm = new CartComponentViewModel();
+
+        }
+
     }
 }
