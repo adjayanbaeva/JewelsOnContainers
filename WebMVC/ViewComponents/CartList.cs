ee4a7910ec3fa211f969aca53fb8f2eeeb6fd960
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-20 22:48:29 -0800

    add CartList ViewComponent and inject ICartService in the constructor of the class

---
+++ WebMVC/ViewComponents/CartList.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVC.Services;
+
+namespace WebMVC.ViewComponents
+{
+    public class CartList : ViewComponent
+    {
+        private readonly ICartService _cartSvc;
+
+        public CartList(ICartService cartSvc)
+        {
+            _cartSvc = cartSvc
+        }
+
+    }
+}
