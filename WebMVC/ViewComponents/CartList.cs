00dace4b514bc73564ab172a59af3608138a9571
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-22 21:14:25 -0800

    add InvokeAsync() method in CartLIst.cs view component

--- WebMVC/ViewComponents/CartList.cs
+++ WebMVC/ViewComponents/CartList.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Polly.CircuitBreaker;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMVC.Models;
 using WebMVC.Services;
 
 namespace WebMVC.ViewComponents
@@ -13,7 +15,26 @@ namespace WebMVC.ViewComponents
 
         public CartList(ICartService cartSvc)
         {
-            _cartSvc = cartSvc
+            _cartSvc = cartSvc;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(ApplicationUser user)
+        {
+            var vm = new Models.CartModels.Cart();
+            try
+            {
+                vm = await _cartSvc.GetCart(user);
+
+                return View(vm);
+            }
+            catch (BrokenCircuitException)
+            {
+                // Catch error when CartApi is in open circuit mode
+                ViewBag.IsBasketInoperative = true;
+                TempData["BasketInoperativeMsg"] = "Basket Service is inoperative, please try later on. (Business Msg Due to Circuit-Breaker)";
+            }
+
+            return View(vm);
         }
 
     }
