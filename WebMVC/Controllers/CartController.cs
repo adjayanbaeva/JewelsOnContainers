0ec23ec89b0942d86557c7b63fced813e06f1586
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-23 22:14:24 -0800

    add Index method to CartController alon with HandleBrokenCircuitException() helper method

--- WebMVC/Controllers/CartController.cs
+++ WebMVC/Controllers/CartController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Polly.CircuitBreaker;
 using WebMVC.Models;
 using WebMVC.Services;
+using Polly.CircuitBreaker;
 
 namespace WebMVC.Controllers
 {
@@ -22,6 +24,34 @@ namespace WebMVC.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Dictionary<string, int> quantities, string action)
+        {
+            if (action == "[Checkout]")
+            {
+                return RedirectToAction("Create", "Order");
+            }
+
+            try
+            {
+                var user = _identityService.Get(HttpContext.User);
+                var basket = await _cartService.SetQuantities(user, quantities);
+                var vm = await _cartService.UpdateCart(basket);
+            }
+            catch (BrokenCircuitException)
+            {
+                // Catch error when CartApi is in open circuit mode
+                HandleBrokenCircuitException();
+            }
+
+            return View();
+        }
+
+        private void HandleBrokenCircuitException()
+        {
+            TempData["BasketInoperativeMsg"] = "cart Service is inoperative, please try later on. (Business Msg Due to Circuit-Breaker)";
         }
     }
 }
