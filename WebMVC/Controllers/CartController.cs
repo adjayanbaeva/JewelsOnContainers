214696e6d75aa0994811124b44119de4882fb18d
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-23 17:18:30 -0800

    add CartController in WebMVC project, inject ICartService, ICatalogService and IIdentityService into the constructor of the CartController

---
+++ WebMVC/Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
+using WebMVC.Services;
+
+namespace WebMVC.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        private readonly ICatalogService _catalogService;
+        private readonly IIdentityService<ApplicationUser> _identityService;
+        public CartController(IIdentityService<ApplicationUser> identityService, ICartService cartService, ICatalogService catalogService)
+        {
+            _identityService = identityService;
+            _cartService = cartService;
+            _catalogService = catalogService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
