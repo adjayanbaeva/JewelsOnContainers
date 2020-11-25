d47077db2fc7bd6458d6b17b33fad36742b0ed31
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-11-24 20:40:26 -0800

    create OrderItem model in OrderApi project and define its properties

---
+++ OrderApi/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApi.Models
+{
+    public class OrderItem
+    {
+        public string ProductName { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Units { get; set; }
+        public int ProductId { get; set; }
+        public Order Order { get; set; }
+        public int OrderId { get; set; }
+    }
+}
