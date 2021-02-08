2888683a674806721616d187179436c8d306aaa0
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2021-02-07 17:49:58 -0800

    add properties within ordercontext class, create a constructor which will accept options and pass it to the base class

--- OrderApi/Data/OrdersContext.cs
+++ OrderApi/Data/OrdersContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OrderApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,14 @@ namespace OrderApi.Data
 {
     public class OrdersContext : DbContext
     {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public OrdersContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
     }
 }
