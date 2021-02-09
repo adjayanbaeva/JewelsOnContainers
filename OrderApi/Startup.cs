dd07711bf7e6d2455a83620f4108e414d57519ec
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2021-02-09 15:32:42 -0800

    define database configuration in Startup of OrderApi and and create a connection string to database

--- OrderApi/Startup.cs
+++ OrderApi/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OrderApi.Data;
 
 namespace OrderApi
 {
@@ -26,6 +28,13 @@ namespace OrderApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            var server = Configuration["DatabaseServer"];
+            var database = Configuration["DatabaseName"];
+            var user = Configuration["DatabaseUser"];
+            var password = Configuration["DatabasePassword"];
+            var connectionString = $"Server={server};Database={database};User Id={user};Password={password}";
+            services.AddDbContext<OrdersContext>(options =>
+                    options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
