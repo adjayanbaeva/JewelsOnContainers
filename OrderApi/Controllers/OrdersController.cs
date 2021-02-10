20e594cfe7054eb4fd6fa3e4da5fe7147058e0d4
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2021-02-09 17:57:04 -0800

    create OrdersController.cs controller in OrderApi and add a constructor, inject dependencies within it

---
+++ OrderApi/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OrderApi.Data;
+
+namespace OrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class OrdersController : ControllerBase
+    {
+        private readonly OrdersContext _ordersContext;
+        private readonly IConfiguration _config;
+        private readonly ILogger<OrdersController> _logger;
+        public OrdersController(OrdersContext ordersContext,
+            ILogger<OrdersController> logger,
+            IConfiguration config)
+        {
+            _config = config;
+            _ordersContext = ordersContext ?? throw new ArgumentNullException(nameof(ordersContext));
+            ordersContext.ChangeTracker.QueryTrackingBehavior = Microsoft.EntityFrameworkCore.QueryTrackingBehavior.NoTracking;
+            _logger = logger;
+        }
+    }
+}
