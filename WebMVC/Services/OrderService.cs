a9be86167cb9885ec9661dd562c1da594f56273a
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2021-02-17 22:54:07 -0800

    create OrderService class that will implement IOrderService interface, inject necessary dependencies into its constructor

---
+++ WebMVC/Services/OrderService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVC.Infrastructure;
+using WebMVC.Models.OrderModels;
+
+namespace WebMVC.Services
+{
+    public class OrderService : IOrderService
+    {
+        private IHttpClient _apiClient;
+        private readonly string _remoteServiceBaseUrl;
+        private readonly IConfiguration _config;
+        private readonly IHttpContextAccessor _httpContextAccesor;
+        private readonly ILogger _logger;
+        public OrderService(IConfiguration config,
+            IHttpContextAccessor httpContextAccesor,
+            IHttpClient httpClient, ILoggerFactory logger)
+        {
+            _remoteServiceBaseUrl = $"{config["OrderUrl"]}/api/orders";
+            _config = config;
+            _httpContextAccesor = httpContextAccesor;
+            _apiClient = httpClient;
+            _logger = logger.CreateLogger<OrderService>();
+        }
+
+        public Task<int> CreateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrder(string orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
