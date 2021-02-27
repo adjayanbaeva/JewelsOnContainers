5aa5df2f8d6ea95573543e22d10e0374981772c2
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2021-02-26 23:40:27 -0800

    create OrderController and inject dependencies in its constructor

---
+++ WebMVC/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using WebMVC.Models;
+using WebMVC.Services;
+
+namespace WebMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ICartService _cartSvc;
+        private readonly IOrderService _orderSvc;
+        private readonly IIdentityService<ApplicationUser> _identitySvc;
+        private readonly ILogger<OrderController> _logger;
+        private readonly IConfiguration _config;
+        public OrderController(IConfiguration config,
+            ILogger<OrderController> logger,
+            IOrderService orderSvc,
+            ICartService cartSvc,
+            IIdentityService<ApplicationUser> identitySvc)
+        {
+            _identitySvc = identitySvc;
+            _orderSvc = orderSvc;
+            _cartSvc = cartSvc;
+            _logger = logger;
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
