e4fa9aac9adc0a53c1424d2e78e281ba1ff589a7
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2021-03-10 23:19:00 -0800

    in cart api project create a new class called OrderCompletedeventConsumer which is IConsumer to listen to published messages

---
+++ CartApi/Messaging/Consumers/OrderCompletedEventConsumer.cs
@@ -0,0 +1,28 @@
+﻿using CartApi.Models;
+using Common.Messaging;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartApi.Messaging.Consumers
+{
+    public class OrderCompletedEventConsumer : IConsumer<OrderCompletedEvent>
+    {
+        private readonly ICartRepositary _repository;
+       
+        public OrderCompletedEventConsumer(ICartRepositary repository)
+        {
+            _repository = repository;
+            
+        }
+
+        public async Task Consume(ConsumeContext<OrderCompletedEvent> context)
+        {
+           
+            await _repository.DeleteCartAsync(context.Message.BuyerId);
+
+        }
+    }
+}
